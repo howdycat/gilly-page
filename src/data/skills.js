284ebfb8577d2b6292a60f02254855bce9338afc
gilly <cg.gillison@gmail.com>
2023-07-26 13:59:48 -0400

    updating image paths

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,80 +1,80 @@
 export const languages = [
     {
         name: 'TypeScript',
-        image: 'public/assets/skills/TypeScript.svg'
+        image: '/skills/TypeScript.svg'
     },
     {
         name: 'JavaScript',
-        image: 'public/assets/skills/JavaScript.svg'
+        image: '/skills/JavaScript.svg'
     },
     {
         name: 'Java',
-        image: 'public/assets/skills/Java.svg'
+        image: '/skills/Java.svg'
     },
     {
         name: 'C#',
-        image: 'public/assets/skills/CSharp.svg'
+        image: '/skills/CSharp.svg'
     },
     {
         name: 'HTML',
-        image: 'public/assets/skills/HTML.svg'
+        image: '/skills/HTML.svg'
     },
     {
         name: 'CSS',
-        image: 'public/assets/skills/CSS.svg'
+        image: '/skills/CSS.svg'
     },
     {
         name: 'C',
-        image: 'public/assets/skills/C.svg'
+        image: '/skills/C.svg'
     }
 ]
 
 export const codeTools = [
     {
         name: 'Bootstrap',
-        image: 'public/assets/skills/Bootstrap.svg'
+        image: '/skills/Bootstrap.svg'
     },
     {
         name: 'Angular 2',
-        image: 'public/assets/skills/Angular.svg'
+        image: '/skills/Angular.svg'
     },
     {
         name: 'Storybook',
-        image: 'public/assets/skills/Storybook.svg'
+        image: '/skills/Storybook.svg'
     },
     {
         name: 'React',
-        image: 'public/assets/skills/React.svg'
+        image: '/skills/React.svg'
     },
     {
         name: 'Tailwind CSS',
-        image: 'public/assets/skills/Tailwind.svg'
+        image: '/skills/Tailwind.svg'
     },
     {
         name: 'Node.js',
-        image: 'public/assets/skills/NodeJS.svg'
+        image: '/skills/NodeJS.svg'
     },
     {
         name: 'Vue.js',
-        image: 'public/assets/skills/VueJS.svg'
+        image: '/skills/VueJS.svg'
     }
 ]
 
 export const designSkills = [
     {
         name: 'Web Design',
-        image: 'public/assets/skills/Web.svg'
+        image: '/skills/Web.svg'
     },
     {
         name: 'UI / UX',
-        image: 'public/assets/skills/Ux.svg'
+        image: '/skills/Ux.svg'
     },
     {
         name: 'Web Animation',
-        image: 'public/assets/skills/Animate.svg'
+        image: '/skills/Animate.svg'
     },
     {
         name: '3D Modeling',
-        image: 'public/assets/skills/Blender.svg'
+        image: '/skills/Blender.svg'
     }
 ]
